8b6603c731b693607c134bd851b52e8b8a2dd3bd
Natalia Martín <nmzaballos@gmail.com>
2021-06-08 11:03:49 +0200

    almost ready!

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import "../stylesheet/App.scss";
 import React, { Component } from "react";
+import data from "../data/data.json";
+import PokeList from "./PokeList";
 
+console.log(data);
 class App extends React.Component {
-  constructor(props) {}
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataList: data,
+    };
+  }
 
   render() {
-    return;
+    return (
+      <>
+        <h1 className="title">Mi lista de Pokemon</h1>;
+        <PokeList list={this.state.dataList} />
+      </>
+    );
   }
 }
 

b80ba68d074a4325377ab4ebb6251cb87e345d71
Natalia Martín <nmzaballos@gmail.com>
2021-06-08 08:55:47 +0200

    template created

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "../stylesheet/App.scss";
+import React, { Component } from "react";
+
+class App extends React.Component {
+  constructor(props) {}
+
+  render() {
+    return;
+  }
+}
+
+export default App;
